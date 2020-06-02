1528060ab6967fd5a6a82e4fa481718dc8cb4ec3
Drowrin <drowrin@gmail.com>
2020-06-02 12:39:20 -0400

    add simple build scripts

---
+++ build.bat
@@ -0,0 +1,2 @@
+elm make src/Main.elm --optimize --output build/main.js
+elm make src/Search.elm --optimize --output build/search.js
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+elm make src/Main.elm --optimize --output build/main.js
+elm make src/Search.elm --optimize --output build/search.js
